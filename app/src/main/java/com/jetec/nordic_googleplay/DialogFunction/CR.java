cf2853aec483ed03f78e135e9f7df61c9e2cca02
smartgamegoy <smartgamegoy@gmail.com>
2019-06-14 17:00:32 +0800

    finish to add new model

--- app/src/main/java/com/jetec/nordic_googleplay/DialogFunction/CR.java
+++ app/src/main/java/com/jetec/nordic_googleplay/DialogFunction/CR.java
@@ -1,10 +1,14 @@
 package com.jetec.nordic_googleplay.DialogFunction;
 
 import android.app.Dialog;
+import android.util.Log;
+
 import com.jetec.nordic_googleplay.SendValue;
 import com.jetec.nordic_googleplay.Service.BluetoothLeService;
 
 public class CR {
+
+    private String TAG = "CR";
 
     public CR() {
         super();
@@ -34,14 +38,25 @@ public class CR {
                 }
                 inDialog.dismiss();
             } else {
+                String out;
                 gets = String.valueOf(t);
                 int i = gets.indexOf(".");
                 String num1 = gets.substring(0, gets.indexOf("."));
                 String num2 = gets.substring(gets.indexOf("."), gets.indexOf(".") + 2);
                 StringBuilder set = new StringBuilder("0");
-                for (int j = 1; j < (4 - i); j++)
+                Log.e(TAG, "i = " + i);
+                Log.e(TAG, "num1 = " + num1);
+                Log.e(TAG, "num2 = " + num2);
+                for (int j = 1; j < (4 - i); j++) {
+                    Log.e(TAG, "j = "+ j);
                     set.append("0");
-                String out = name + "+" + set + num1 + num2;
+                }
+                if(i < 4) {
+                    out = name + "+" + set + num1 + num2;
+                }
+                else {
+                    out = name + "+" + num1 + num2;
+                }
                 sendValue.send(out);
                 inDialog.dismiss();
             }
--- app/src/main/java/com/jetec/nordic_googleplay/DialogFunction/EL.java
+++ app/src/main/java/com/jetec/nordic_googleplay/DialogFunction/EL.java
@@ -2,12 +2,15 @@ package com.jetec.nordic_googleplay.DialogFunction;
 
 import android.app.Dialog;
 import android.content.Context;
+import android.util.Log;
 import android.widget.Toast;
 import com.jetec.nordic_googleplay.R;
 import com.jetec.nordic_googleplay.SendValue;
 import com.jetec.nordic_googleplay.Service.BluetoothLeService;
 
 public class EL {
+
+    private String TAG = "EL";
 
     private Context context;
 
@@ -41,14 +44,25 @@ public class EL {
                     }
                     inDialog.dismiss();
                 } else {
+                    String out;
                     gets = String.valueOf(t);
                     int i = gets.indexOf(".");
                     String num1 = gets.substring(0, gets.indexOf("."));
                     String num2 = gets.substring(gets.indexOf("."), gets.indexOf(".") + 2);
                     StringBuilder set = new StringBuilder("0");
-                    for (int j = 1; j < (4 - i); j++)
+                    Log.e(TAG, "i = " + i);
+                    Log.e(TAG, "num1 = " + num1);
+                    Log.e(TAG, "num2 = " + num2);
+                    for (int j = 1; j < (4 - i); j++) {
+                        Log.e(TAG, "j = "+ j);
                         set.append("0");
-                    String out = name + "+" + set + num1 + num2;
+                    }
+                    if(i < 4) {
+                        out = name + "+" + set + num1 + num2;
+                    }
+                    else {
+                        out = name + "+" + num1 + num2;
+                    }
                     sendValue.send(out);
                     inDialog.dismiss();
                 }
